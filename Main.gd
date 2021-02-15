cc9de96a4fc9924965131bbdc561dc464c01adeb
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-02-15 16:40:08 -0500

    feat: feet

--- Main.gd
+++ Main.gd
@@ -22,5 +22,26 @@ func _input(_event):
 		movement = Vector2(movement.x, 0)
 
 const _COEFF = 50.0
+var _moving_foot = Foot.LEFT
+enum Foot { LEFT, RIGHT }
+var _step_progress = 0
+const _MAX_STEP = 100
 func _physics_process(_delta):
-	$Player/KinUpperBody.move_and_slide(Vector3(movement.x * _COEFF, 0, movement.y * _COEFF))
+	var upper_body = $Player/KinUpperBody
+	
+	var v = Vector3(movement.x * _COEFF, 0, movement.y * _COEFF)
+	upper_body.move_and_slide(v)
+	
+	_foot_node().move_and_slide(2.0 * v)
+	_step_progress = (_step_progress + 1) % _MAX_STEP
+	if _step_progress == 0:
+		_change_foot()
+func _change_foot():
+	_moving_foot = Foot.RIGHT if _moving_foot == Foot.LEFT else Foot.LEFT
+
+func _foot_node():
+	if _moving_foot == Foot.LEFT:
+		return $Player/LeftFoot
+	else:
+		return $Player/RightFoot
+	
